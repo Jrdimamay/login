9586a6697597d43b9da8487957f1cb12ac67ae05
Jrdimamay <jrdimay@gmail.com>
2024-12-27 11:52:50 +0800

    Create connections.php

---
+++ connections.php
@@ -0,0 +1,8 @@
+<?php
+$connections = mysqli_connect("localhost:3307", "root", "", "dimamay");
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+    echo "";                             
+}
+
